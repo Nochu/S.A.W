25fe735cda384c065e7414b03858fb05355c8798
Nochu <hot1087@naver.com>
2016-04-11 21:54:35 +0900

    Ant, Canon

---
+++ Assets/Scripts/AntScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntScript : MonoBehaviour {
+
+	float fSpeed = 0.5f;	// 개미의 이동 속도
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float fAmtToMove = fSpeed * Time.smoothDeltaTime;	// 프레임에 이동할 거리
+
+		transform.Translate(Vector3.left * fAmtToMove);		// 개미 이동
+	}
+}
---
+++ Assets/Scripts/TurretScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretScript : MonoBehaviour {
+
+	float fRotSpeed = 200f;	// 포탑의 회전속도
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float fAmtToRot = fRotSpeed * Time.smoothDeltaTime;	// 프레임에 이동할 거리
+		float fAngle	= Input.GetAxis( "Vertical" );		// 포탑 각도 방향(벡터)
+
+		transform.Rotate(Vector3.forward * fAngle * fAmtToRot);	// 포탑 각도 조절
+	}
+}
